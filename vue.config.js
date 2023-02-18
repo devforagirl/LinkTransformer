1db3050fe89f4f99a2a2ff8ffd2e440dbe85671d
devforagirl <48156801+devforagirl@users.noreply.github.com>
2023-02-18 06:35:09 +0000

    To fix the "Invalid Host/Origin header" error

--- vue.config.js
+++ vue.config.js
@@ -7,5 +7,9 @@ module.exports = {
     if (process.env.NODE_ENV === 'production') {
       config.optimization.minimizer[0].options.terserOptions.compress.drop_console = true
     }
+  },
+  // To fix the "Invalid Host/Origin header" error
+  devServer: {
+    disableHostCheck: true
   }
 };
